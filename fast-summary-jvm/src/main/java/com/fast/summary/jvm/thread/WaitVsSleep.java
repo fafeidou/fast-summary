fa2340d1cf7b0ff4a1c23e7db201f9782dda6c70
qinfuxiang <batman.qin@huolala.cn>
2022-02-11 10:47:03 +0800

    test WaitVsSleep

---
+++ fast-summary-jvm/src/main/java/com/fast/summary/jvm/thread/WaitVsSleep.java
@@ -0,0 +1,59 @@
+package com.fast.summary.jvm.thread;
+
+
+import static com.fast.summary.jvm.utils.LoggerUtils.get;
+import static com.fast.summary.jvm.utils.LoggerUtils.main;
+
+public class WaitVsSleep {
+    static final Object LOCK = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        sleeping();
+    }
+
+    private static void illegalWait() throws InterruptedException {
+        LOCK.wait();
+    }
+
+    private static void waiting() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            synchronized (LOCK) {
+                try {
+                    get("t").debug("waiting...");
+                    LOCK.wait(5000L);
+                } catch (InterruptedException e) {
+                    get("t").debug("interrupted...");
+                    e.printStackTrace();
+                }
+            }
+        }, "t1");
+        t1.start();
+
+        Thread.sleep(100);
+        synchronized (LOCK) {
+            main.debug("other...");
+        }
+
+    }
+
+    private static void sleeping() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            synchronized (LOCK) {
+                try {
+                    get("t").debug("sleeping...");
+                    Thread.sleep(5000L);
+                } catch (InterruptedException e) {
+                    get("t").debug("interrupted...");
+                    e.printStackTrace();
+                }
+            }
+        }, "t1");
+        t1.start();
+
+        Thread.sleep(100);
+        t1.interrupt();
+        /*synchronized (LOCK) {
+            main.debug("other...");
+        }*/
+    }
+}
